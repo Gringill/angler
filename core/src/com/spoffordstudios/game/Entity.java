86f6fbd2df21e80c4f0a68da34786b753711ed2c
Daniel Spofford <dannyspofford@gmail.com>
2014-07-16 13:14:53 -0500

    Change Package Names

---
+++ core/src/com/spoffordstudios/game/Entity.java
@@ -0,0 +1,15 @@
+package com.spoffordstudios.game;
+
+public class Entity {
+	public void update() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void preload() {
+		
+	}
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,13 +2,15 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.main;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.spoffordstudios.game.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
+		TexturePacker.process("../core/assets/unpacked", "../core/assets", Main.ATLAS_NAME);
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 960;
-		config.height = 540;
-		new LwjglApplication(new main(), config);
+		config.width = 1920/2;
+		config.height = 1080/2;
+		new LwjglApplication(new Main(), config);
 	}
 }
