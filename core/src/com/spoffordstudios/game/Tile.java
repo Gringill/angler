60439538e39d33818afa38d06ec6b6c5cd7f417e
Daniel Spofford <dannyspofford@gmail.com>
2014-07-22 15:08:40 -0500

    Core game functionality added
    Added game interface, and tile classes. Renamed Main to Game.

---
+++ core/src/com/spoffordstudios/game/Tile.java
@@ -0,0 +1,10 @@
+package com.spoffordstudios.game;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class Tile extends Entity {
+
+	public Tile(String definition, Sprite sprite, int x, int y) {
+		super(definition, sprite, x, y);
+	}
+}
